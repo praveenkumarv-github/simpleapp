c236daab8ca88b8841d3d29389e9e9cd29135fa9
root <root@master.localdomain>
2024-01-11 01:03:02 -0500

    Simple app created with Dokcerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use the official Python image with Alpine Linux
+FROM python:3.10-alpine
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the Python script and HTML file into the container
+COPY main.py /app/main.py
+COPY index.html /app/index.html
+
+# Expose the port that the app will run on
+EXPOSE 8000
+
+# Install any dependencies (not required in this case as it's a simple example)
+# RUN pip install some-package
+
+# Command to run the web app when the container starts
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleWebAppHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Read HTML content from file
+        with open('index.html', 'r') as file:
+            html_content = file.read()
+
+        self.wfile.write(html_content.encode('utf-8'))
+
+def run_web_app(port=8000):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, SimpleWebAppHandler)
+    print('Starting web app on port {}'.format(port))
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_web_app()
